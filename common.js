20d6634e635f2d17e2cb99ba27e387859e03a150
Vadim Belorussov <belorussov.vadim@gmail.com>
2017-07-07 14:45:22 +0500

    Add common functions

---
+++ common.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = {
+    executeCommand: executeCommand
+};
+
+/**
+ * Execute specified command request to WebEx URL API.
+ * @param {string} command
+ * @param {object} params
+ * @returns {Promise}
+ */
+function executeCommand(command, params) {
+    setCommand(command, params);
+    params = setParams(params);
+    return apiRequest(params);
+}
+
+/**
+ * Set "AT" parameter of params.
+ * @param {string} command
+ * @param {object} params
+ */
+function setCommand(command, params) {
+    // here we process command
+    params.AT = command;
+}
+
+/**
+ * Process params of command.
+ * @param {object} params
+ */
+function setParams(params) {
+    // here we process params
+    return params;
+}
+
+/**
+ * Make API request to WebEx URL API with specified params.
+ * @param params
+ * @return {Promise}
+ */
+function apiRequest(params) {
+    return Promise.resolve(params);
+}
