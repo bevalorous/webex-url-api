ca9e71ba718acce4ba32f00badfd6ad9e64bdc1e
Vadim Belorussov <belorussov.vadim@gmail.com>
2017-07-07 15:44:29 +0500

    Add commands "enrollEvent" and "joinEvent" to EventCenter

--- services/event-center/index.js
+++ services/event-center/index.js
@@ -1,9 +1,28 @@
 "use strict";
+
 const webExHostedWebsite = require("../../common").webExHostedWebsite;
+var executeCommand = require("./common").executeCommand;
 
 module.exports = EventCenter;
 
 function EventCenter(yourWebExHostedName) {
     webExHostedWebsite.apply(this, arguments);
+
     // set commands available to EventCenter
+    this.enrollEvent = enrollEvent;
+    this.joinEvent = joinEvent;
+}
+
+/**
+ * @param {object} params
+ */
+function enrollEvent(params) {
+    return executeCommand("EN", params);
+}
+
+/**
+ * @param {object} params
+ */
+function joinEvent(params) {
+    return executeCommand("JE", params);
 }
